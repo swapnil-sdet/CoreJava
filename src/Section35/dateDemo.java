d6ca908caaffbc62aa33e3cbac85d82e4c90da62
SwapnilPadekar <gitspace.sdet@gmail.com>
2024-01-16 18:57:52 +0530

    Date related changes

---
+++ src/Section35/dateDemo.java
@@ -0,0 +1,18 @@
+package Section35;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class dateDemo {
+
+    public static void main(String[] args) {
+
+        Date d = new Date();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd:MM:yyyy hh:mm:ss");
+        System.out.println(sdf.format(d));
+        System.out.println(d.toString());
+
+
+    }
+}
