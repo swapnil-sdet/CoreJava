3120810d19de865973bedf4a38ad9fefff18bc3b
SwapnilPadekar <gitspace.sdet@gmail.com>
2024-03-05 18:07:24 +0530

    Learning for Section 35

--- src/Section38/hashSetExample.java
+++ src/Section38/hashSetExample.java
@@ -4,7 +4,7 @@ public class hashSetExample {
     public static void main(String[] args) {
         //HashSet, TreeSet, LinkedHahSet implements Set interface
         //doesn't accept duplicate values
-        //there is no guarentee that the elements will be stored in given sequence
+        //there is no guarantee that the elements will be stored in given sequence
 
 
 
